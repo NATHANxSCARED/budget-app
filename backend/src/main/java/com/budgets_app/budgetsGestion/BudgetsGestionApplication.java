5f58a010e8ae956efa5ff06ba56b26c5e1e4a81b
Scared <nathan@vittoni.ch>
2025-05-05 22:54:40 +0200

    inital comit for sprin and docker postgrey sql and ionic for front

---
+++ backend/src/main/java/com/budgets_app/budgetsGestion/BudgetsGestionApplication.java
@@ -0,0 +1,13 @@
+package com.budgets_app.budgetsGestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetsGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetsGestionApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/budgets_app/budgetsGestion/BudgetsGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgets_app.budgetsGestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetsGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
