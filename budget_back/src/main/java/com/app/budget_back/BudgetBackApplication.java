1c42db8d88b63a4ffcb35a87ea2242dcd661f915
Scared <nathan@vittoni.ch>
2025-05-10 22:06:05 +0200

    spring marche yes !

--- budget_back/src/main/java/com/app/budget_back/BudgetBackApplication.java
+++ budget_back/src/main/java/com/app/budget_back/BudgetBackApplication.java
@@ -1,7 +1,13 @@
 package com.app.budget_back;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class BudgetBackApplication {
@@ -9,5 +15,6 @@ public class BudgetBackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetBackApplication.class, args);
 	}
+
 
 }
---
+++ budget_back/src/main/java/com/app/budget_back/budgets/Budgets.java
@@ -0,0 +1,5 @@
+package com.app.budget_back.budgets;
+
+public class Budgets {
+
+}
