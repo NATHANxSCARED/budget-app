61179151cf9ecbf8f784c30174c587434471f03c
Nathan Vittoni <nathan.vittoni@etat.ge.ch>
2025-05-19 15:11:16 +0200

    page acount

--- frontend/src/pages/Tab3.tsx
+++ frontend/src/pages/Tab3.tsx
@@ -1,22 +1,34 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import Acount from "../components/Acount";
+import styles from "./Tab3.module.css";
 
 const Tab3: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Acount</IonTitle>
+          <IonTitle>Account</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+
+        <div className={styles["tab3-container"]}>
+          <div className={styles["card-wrapper"]}>
+            <Acount />
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
